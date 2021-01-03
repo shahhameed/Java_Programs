25163e096c62bd0c5c21a3018f6fa3a39b1c1deb
shahhameed <shahwarishameed@gmail.com>
2021-01-03 23:40:43 +0530

    Added Literals

---
+++ src/com/company/SH_02_Literals.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class SH_02_Literals {
+    public static void main(String[] args) {
+        byte age = 25;
+        int age2 = 26;
+        short age3 = 29;
+        long ageDino = 5666666666666L;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66;
+
+        boolean a = true;
+        System.out.print(age);
+        String str = ("\n" +
+                "Shah").formatted();
+        System.out.println(str);
+    }
+}
